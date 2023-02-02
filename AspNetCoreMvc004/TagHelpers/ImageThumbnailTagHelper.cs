85d8432c174499d9f41050f685fb9d0b0e343372
Umutcan Adiguzel <106314261+umtt31@users.noreply.github.com>
2023-02-02 17:30:49 +0300

    tag helperlar eklendi price icin regular expression eklendi

---
+++ AspNetCoreMvc004/TagHelpers/ImageThumbnailTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AspNetCoreMvc004.TagHelpers
+{
+    public class ImageThumbnailTagHelper :TagHelper
+    {
+        public string ImageSrc { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "img";  
+
+            string fileName = ImageSrc.Split('.')[0];
+            string fileExtention = Path.GetFileName(ImageSrc); 
+
+            output.Attributes.SetAttribute("src", $"{fileName}-100x100{fileExtention}");
+        }
+    }
+}
---
+++ AspNetCoreMvc004/TagHelpers/productShowTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using AspNetCoreMvc004.Models;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace AspNetCoreMvc004.TagHelpers
+{ 
+    public class productShowTagHelper : TagHelper
+    {
+        public Product Product { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "<div>";
+            output.Content.SetHtmlContent($@"<ul class='list-group'>
+                                                <li class=''list-group-item>{Product.Id}</li>
+                                                <li class=''list-group-item>{Product.Name}</li>
+                                                <li class=''list-group-item>{Product.Price}</li>
+                                                <li class=''list-group-item>{Product.Stock}</li>
+                                            </ul>");
+        }
+
+    }
+}
