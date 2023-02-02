96b1e0f2bd7d841b5f77e58d8ec7d328bbf48224
Umutcan Adiguzel <106314261+umtt31@users.noreply.github.com>
2023-02-02 14:19:01 +0300

    Table view component i eklendi farkli veri aliyor ve verilerle tabloyu dolduruyor

--- AspNetCoreMvc004/Views/Shared/ViewComponents/VisitorViewComponent.cs
+++ AspNetCoreMvc004/Views/Shared/ViewComponents/VisitorViewComponent.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreMvc004.Models;
+using AspNetCoreMvc004.ViewModels;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreMvc004.Views.Shared.ViewComponents
 {
     public class VisitorViewComponent : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync()
+
+        private readonly AppDbContext _context;
+
+        private readonly IMapper _mapper;
+
+        public VisitorViewComponent(AppDbContext context, IMapper mapper)
         {
-            return View();
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string type)
+        { 
+            if (type == "table")
+            {
+                var visitors = _context.Visitors.ToList();
+                var visitorsViewModel = _mapper.Map<List<VisitorViewModel>>(visitors);
+
+                return View("Table", visitorsViewModel);
+            } 
+            else
+            {
+                return View("Default");
+            }
+            
         }
     }
 }
