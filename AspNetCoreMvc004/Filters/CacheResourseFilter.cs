faf5b5f675efe6d2456a9e2764ae033022649cd1
Umutcan Adiguzel <106314261+umtt31@users.noreply.github.com>
2023-02-07 14:21:31 +0300

    cacheleme islemi yapildi exception handler kondo sesult filter eklendi

---
+++ AspNetCoreMvc004/Filters/CacheResourseFilter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AspNetCoreMvc004.Filters
+{
+    public class CacheResourseFilter : Attribute, IResourceFilter
+    {
+        private static IActionResult _cache;
+
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            _cache = context.Result;
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            context.Result = _cache;
+        }
+    }
+}
---
+++ AspNetCoreMvc004/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,19 @@
+﻿using AspNetCoreMvc004.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore;
+
+namespace AspNetCoreMvc004.Filters
+{
+    public class CustomExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            context.ExceptionHandled = true;
+
+            var error = context.Exception.Message;
+
+            context.Result = new RedirectToActionResult("error", "home", new ErrorViewModel() { Errors = new List<string> { "Unknown Error" });
+        }
+    }
+}
